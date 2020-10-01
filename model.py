60b249ce5a5264e2a6accc623839d8a3a58efe02
AJB117 <psoga117@gmail.com>
2020-10-01 00:45:39 -0400

    finished preliminary model; lots of cleaning to do still

---
+++ model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+
+df = pd.read_csv("./data/integrated_features_and_labels.csv", engine="python")
+
+# some (heavy) preprocessing
+le = LabelEncoder()
+df['PARTY'] = le.fit_transform(df['PARTY'].values)
+df = df.drop(['REDIST', 'ACEREGIO', 'CONSTRUC', 'CVLLBRFR', 'CANDIDATEVOTES', 'TOTALVOTES', 'ID', 'PORT', 'YEAR', 'STATE', 
+              'PREVID', 'PREVCANDVO', 'PREVTOTVO', 'FIPSTATE', 'SC', 'CD', 'PREVPARTY', 'NUCPLANT', 'POPSQMI', 'MDNINCM',
+              'MANUF', 'WHLRETL', 'VETERANS', 'VABEDS', 'TRANSPRT'], 1)
+
+# test/train split
+y = df['PARTY']
+x = df.drop(['PARTY'], 1)
+# print(np.where(pd.isnull(df)))
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+# print(df.columns[29])
+
+# print(X_train)
+
+# print(np.where(x.applymap(lambda x: x == '')))
+
+# model train and test
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print(f"Accuracy: {metrics.accuracy_score(y_test, y_pred)}")
