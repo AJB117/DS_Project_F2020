f309d1570cfae507235a9d97511648551f659f4d
AJB117 <psoga117@gmail.com>
2020-11-09 01:05:42 -0500

    working on interpretability

--- model_interpretability.py
+++ model_interpretability.py
@@ -32,5 +32,23 @@ clf_forest = RandomForestClassifier()
 clf_boost = AdaBoostClassifier(base_estimator=tree.DecisionTreeClassifier(max_depth=8), random_state=21)
 clf_mlp = MLPClassifier(hidden_layer_sizes=(50,50,), max_iter=1000, tol=0.001, random_state=42)
 
+# test/train split
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2)
+print(Y_train)
+print("No Sampling: Label = flip:", sum(Y_train == 1))
+print("No Sampling: Label = not flip:", sum(Y_train == 0))
+
+# initialize SMOTE oversampling algorithm; oversamples all rows with label not in majority
+sm = SMOTE(random_state=2)
+X_train_OS, Y_train_OS = sm.fit_sample(X_train, Y_train)
+print("SMOTE Oversampling: Label = flip:", sum(Y_train_OS == 1))
+print("SMOTE Oversampling: Label = not flip:", sum(Y_train_OS == 0))
+
+# initialize near miss undersampling algorithm
+nm = NearMiss()
+X_train_US, Y_train_US = nm.fit_sample(X_train, Y_train)
+print("SMOTE Undersampling: Label = flip:", sum(Y_train_US == 1))
+print("SMOTE Undersampling: Label = not flip:", sum(Y_train_US == 0))
+
 for feature in df.columns:
   pass
